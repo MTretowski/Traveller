f4654017c90bc034bc56176e6652aa878b840ce4
MTretowski <m.tretowski@wp.pl>
2018-04-08 19:56:30 +0200

    Poprawka problemu z kodowaniem znaków w wiadomościach błędów

--- src/main/java/pl/traveller/Services/ErrorMessagesService.java
+++ src/main/java/pl/traveller/Services/ErrorMessagesService.java
@@ -2,42 +2,57 @@ package pl.traveller.Services;
 
 import org.springframework.stereotype.Service;
 
-import java.util.Locale;
-import java.util.MissingResourceException;
-import java.util.ResourceBundle;
+import java.io.*;
+import java.util.HashMap;
 
 @Service
 public class ErrorMessagesService {
 
-    private ResourceBundle resourceBundle;
+    private HashMap<String, String> messagesPl = new HashMap<>();
+    private HashMap<String, String> messagesEn = new HashMap<>();
 
     ErrorMessagesService() {
-        resourceBundle = ResourceBundle.getBundle("messages");
+        messagesEn = getMessages("/messages_en.csv");
+        messagesPl = getMessages("/messages_pl.csv");
+    }
+
+    private HashMap<String, String> getMessages(String filePath){
+
+        HashMap<String, String> messages = new HashMap<>();
+
+        InputStream inputStream = this.getClass().getResourceAsStream(filePath);
+        BufferedReader bufferedReader = null;
+        String readedLine;
+
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "UTF8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if (bufferedReader != null) {
+                while ((readedLine = bufferedReader.readLine()) != null) {
+                    String[] splittedLine = readedLine.split(";");
+                    messages.put(splittedLine[0], splittedLine[1]);
+
+                }
+            }
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return messages;
+
     }
 
     String getErrorMessage(String language, String key) {
-
-        String error;
-
-        if(!(language.equals("pl") || language.equals("en"))){
-            language = "en";
+        if(language.equals("pl")){
+            return messagesPl.get(key);
         }
-        if (!resourceBundle.getLocale().toString().equals(language)) {
-            Locale.setDefault(new Locale(language));
-            resourceBundle = ResourceBundle.getBundle("messages");
+        else{
+            return messagesEn.get(key);
         }
 
-        try{
-            error = resourceBundle.getString(key);
-        }catch(MissingResourceException e){
-            if(language.equals("pl")){
-                error = "Nieznany błąd";
-            }
-            else{
-                error = "Unknown error";
-            }
-        }
-
-        return error;
     }
 }
